9e71147733a21b027c13f5d4179cd234a40e1f20
Kevin Poelman <56067721+Neo-Phoenix@users.noreply.github.com>
2024-06-03 03:03:06 +0200

    Update navbar interactive gemaakt met js arrow function en defer gebruikt in html script tag defer: https://stackoverflow.com/questions/807878/how-to-make-javascript-execute-after-page-load
    js doc:
    https://www.w3schools.com/jsref/met_document_addeventlistener.asp
    https://www.w3schools.com/js/js_best_practices.asp
    
    https://www.w3schools.com/js/js_htmldom_document.asp
    https://www.w3schools.com/js/js_htmldom_elements.asp
    https://www.w3schools.com/js/js_htmldom_events.asp
    https://www.w3schools.com/js/js_htmldom_eventlistener.asp

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 let ico = document.getElementById('hamburgermenu');
 let nav = document.getElementsByTagName('nav')[0];
 
-ico.addEventListener('click', function() {
-    if (nav.style.display != 'block') {
+ico.addEventListener('click', () => {
+    if (nav.style.display !== 'block') {
         nav.style.display = 'block';
     } else {
         nav.style.display = 'none';
