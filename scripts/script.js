3c164928131554b7158dff2b34b20267ffc57738
Kevin Poelman <56067721+Neo-Phoenix@users.noreply.github.com>
2024-06-05 23:40:31 +0200

    About us pagina toegevoegd, foto toegevoegd, refactor van .gallery naar .gallerySection
    foto: https://www.pexels.com/nl-nl/foto/restaurant-eetgelegenheid-eetcafe-trottoir-15638789/
    
    vertical resize toegepast in textarea form
    https://developer.mozilla.org/en-US/docs/Web/CSS/resize
    
    alle paginas hebben in body nu de pagina tag
    js aangepast om de huidige pagina te kunnen zien

--- scripts/script.js
+++ scripts/script.js
@@ -12,82 +12,88 @@ ico.addEventListener('click', () => {
 
 window.onload = () => {
     console.log("helloworld.")
+    const currentPage = document.getElementsByTagName('body')[0].id
 
-    // Array destructuring assignment van variabelen
-    const [cnext, cprevious, gallerypic] = [
-        document.getElementById('cnext'), 
-        document.getElementById('cprevious'),
-        document.getElementById('gallerypicture')
-]
-    const galleryUrls = [
-        "url(../img/pexels-chevanon-323682.jpg)",
-        "url(../img/pexels-dana-tentis-118658-262959.jpg)",
-        "url(../img/pexels-ella-olsson-572949-1640777.jpg)",
-        "url(../img/pexels-julieaagaard-2097090.jpg)",
-        "url(../img/pexels-rajesh-tp-749235-1633525.jpg",
-        "url(../img/pexels-robinstickel-70497.jpg)",
-        "url(../img/pexels-valeriya-1199957.jpg)",
-        "url(../img/pexels-vanmalidate-769289.jpg)",
-        "url(../img/pexels-willpicturethis-2641886.jpg)"
-    ]
+    console.log(currentPage)
+    if(currentPage === "gallery") {
+        // Array destructuring assignment van variabelen
+        const [cnext, cprevious, gallerypic] = [
+            document.getElementById('cnext'), 
+            document.getElementById('cprevious'),
+            document.getElementById('gallerypicture')
+        ]
+        const galleryUrls = [
+            "url(../img/pexels-chevanon-323682.jpg)",
+            "url(../img/pexels-dana-tentis-118658-262959.jpg)",
+            "url(../img/pexels-ella-olsson-572949-1640777.jpg)",
+            "url(../img/pexels-julieaagaard-2097090.jpg)",
+            "url(../img/pexels-rajesh-tp-749235-1633525.jpg",
+            "url(../img/pexels-robinstickel-70497.jpg)",
+            "url(../img/pexels-valeriya-1199957.jpg)",
+            "url(../img/pexels-vanmalidate-769289.jpg)",
+            "url(../img/pexels-willpicturethis-2641886.jpg)"
+        ]
 
-    //spread operator voegt "cnext, cprevious, gallerypic" samen met de inhoud van gallerUrls
-    const spreadOperatorTest = [cnext, cprevious, gallerypic, ...galleryUrls];
-    console.log(spreadOperatorTest)
+        //spread operator voegt "cnext, cprevious, gallerypic" samen met de inhoud van gallerUrls
+        const spreadOperatorTest = [cnext, cprevious, gallerypic, ...galleryUrls];
+        console.log(spreadOperatorTest)
 
-    if (cnext) {
-        cnext.addEventListener('click', () => {
-            galleryControls("next")
-        })
-    }
-
-    if(cprevious) {
-        cprevious.addEventListener('click', () => {
-            galleryControls("previous")
-        })
-    }
-    let i = 0
-    //Iteratie over een array van urls voor de gallery
-    function galleryControls(direction, ...nuttelozeArgumentenOpgevangenDoorRestOperator) {
-        if (direction === "next") {
-            console.log("next i before" + i)
-            console.log(galleryUrls[i])
-
-            if (i >= galleryUrls.length-1) {
-                i = 0;
-            } else {
-                i++;
-            }
-            console.log("next i current" + i)
-
-            gallerypic.style.backgroundImage = galleryUrls[i]
-            gallerypic.style.transition = "0.25s"
-        } else if (direction === "previous"){
-            console.log("previous i before:" + i)
-            console.log(galleryUrls[i])
-            if (i <= 0) {
-                i = galleryUrls.length-1;
-            } else {
-                i--;
-            }
-            console.log("next i current" + i)
-
-            gallerypic.style.backgroundImage = galleryUrls[i]
-            gallerypic.style.transition = "0.25s"
+        if (cnext) {
+            cnext.addEventListener('click', () => {
+                galleryControls("next")
+            })
         }
-        if (nuttelozeArgumentenOpgevangenDoorRestOperator != "") {
-            //rest operator vangt extra argumenten op en logged ze in console
-            console.log(nuttelozeArgumentenOpgevangenDoorRestOperator)
+
+        if(cprevious) {
+            cprevious.addEventListener('click', () => {
+                galleryControls("previous")
+            })
         }
+        let i = 0
+        //Iteratie over een array van urls voor de gallery
+        function galleryControls(direction, ...nuttelozeArgumentenOpgevangenDoorRestOperator) {
+            if (direction === "next") {
+                console.log("next i before" + i)
+                console.log(galleryUrls[i])
+
+                if (i >= galleryUrls.length-1) {
+                    i = 0;
+                } else {
+                    i++;
+                }
+                console.log("next i current" + i)
+
+                gallerypic.style.backgroundImage = galleryUrls[i]
+                gallerypic.style.transition = "0.25s"
+            } else if (direction === "previous"){
+                console.log("previous i before:" + i)
+                console.log(galleryUrls[i])
+                if (i <= 0) {
+                    i = galleryUrls.length-1;
+                } else {
+                    i--;
+                }
+                console.log("next i current" + i)
+
+                gallerypic.style.backgroundImage = galleryUrls[i]
+                gallerypic.style.transition = "0.25s"
+            }
+            if (nuttelozeArgumentenOpgevangenDoorRestOperator != "") {
+                //rest operator vangt extra argumenten op en logged ze in console
+                console.log(nuttelozeArgumentenOpgevangenDoorRestOperator)
+            }
+        }
+
+        //verwachte output van galleryControls(1,2,'test',4,5,6) is een array [2,'test',4,5,6] in console log
+        galleryControls(1,2,'test',4,5,6)
+
+        //verwachte output van galleryControls(1) is niets in console log
+        galleryControls(1)
+
+        //verwachte output van galleryControls(1) is een array van [2] in console log
+        galleryControls(1,2)
     }
-
-    //verwachte output van galleryControls(1,2,'test',4,5,6) is een array [2,'test',4,5,6] in console log
-    galleryControls(1,2,'test',4,5,6)
-
-    //verwachte output van galleryControls(1) is niets in console log
-    galleryControls(1)
-
-    //verwachte output van galleryControls(1) is een array van [2] in console log
-    galleryControls(1,2)
-
+    else if (currentPage === "about-us") {
+        console.log("test")
+    }
 }
