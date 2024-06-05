bf07d5a62b3f6468b35967830a5cfe3b680cd277
Kevin Poelman <56067721+Neo-Phoenix@users.noreply.github.com>
2024-06-05 05:02:07 +0200

    js functie update gallery, todo: bugfix array out of bounds => undefined

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,18 @@ window.onload = () => {
 
     const cnext = document.getElementById('cnext')
     const cprevious = document.getElementById('cprevious')
+    const gallerypic = document.getElementById('gallerypicture')
+    const galleryUrls = [
+        "url(../img/pexels-chevanon-323682.jpg)",
+        "url(../img/pexels-dana-tentis-118658-262959.jpg)",
+        "url(../img/pexels-ella-olsson-572949-1640777.jpg)",
+        "url(../img/pexels-julieaagaard-2097090.jpg)",
+        "url(../img/pexels-rajesh-tp-749235-1633525.jpg",
+        "url(../img/pexels-robinstickel-70497.jpg)",
+        "url(../img/pexels-valeriya-1199957.jpg)",
+        "url(../img/pexels-vanmalidate-769289.jpg)",
+        "url(../img/pexels-willpicturethis-2641886.jpg)"
+    ]
 
     cnext.addEventListener('click', () => {
         galleryControls("next")
@@ -22,12 +34,18 @@ window.onload = () => {
     cprevious.addEventListener('click', () => {
         galleryControls("previous")
     })
-
+    let i = 0
     function galleryControls(direction) {
         if (direction === "next") {
             console.log("next")
+            console.log(galleryUrls[i])
+            i++
+            gallerypic.style.backgroundImage = galleryUrls[i]
         } else if (direction === "previous"){
             console.log("previous")
+            console.log(galleryUrls[i])
+            i--
+            gallerypic.style.backgroundImage = galleryUrls[i]
         }
     }
 }
